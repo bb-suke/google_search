b767758dd1059c54e1662c55be4fdcd92421b117
hirose <ryousuke_0621@yahoo.co.jp>
2023-04-11 20:29:50 +0900

    google-custom-search-json-apiで使用するapiKeyと検索IDをenvで指定できるよう追記

---
+++ laravel/config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'google_search_json_api' => [
+        'api_key' => env('GOOGLE_SEARCH_JSON_API_KEY', 'AIzaSyClCA6XFuO071dlaemdTHmd6E4dbdHhhnk'),
+        'search_engine_id' => env('GOOGLE_SEARCH_JSON_ENGINE_ID', '1701f15edfe764754')
+    ]
+];
